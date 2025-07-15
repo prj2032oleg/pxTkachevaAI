aa3d0d110cfdf3febca33b3340721bd05750bb2f
prj2032oleg <prj2032.04lst@gmail.com>
2025-07-15 06:34:45 +0200

    EXZP-27

---
+++ assets/js/mouseflow.js
@@ -0,0 +1,8 @@
+window._mfq = window._mfq || [];
+    (function() {
+                         var mf = document.createElement("script");
+                         mf.type = "text/javascript"; mf.defer = true;
+                         mf.src = "//cdn.mouseflow.com/projects/f667d01f-7874-467e-9c55-5aaef297b7da.js";
+                         document.getElementsByTagName("head")[0].appendChild(mf);
+})();
+
